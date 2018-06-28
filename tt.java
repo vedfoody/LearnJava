35db6b568082439a440cdbe0605523bcc043d007
vedfoody <vedfoody@gmail.com>
2018-06-29 00:11:57 +0700

    add new file for java

---
+++ tt.java
@@ -0,0 +1,54 @@
+package helloworld;
+
+public class tt {
+
+	public static void main(String[] args) {
+		System.out.println(countSquarePairs(new int[] {11, 5, 4, 20}));
+		System.out.println(countSquarePairs(new int[] {2, 7, 2, 2}));
+		System.out.println(countSquarePairs(new int[] {9, 0, 2, -5, 7}));
+		System.out.println(countSquarePairs(new int[] {9}));
+		System.out.println(countSquarePairs(new int[] {2, 7, 0, 1, 5, 1}));
+	}
+
+	static int countSquarePairs(int[] a) {
+		if (a.length < 2) {
+			return 0;
+		}
+		
+		int countSquarePairsResult = 0;
+		for (int i = 0; i < a.length - 1; i++) {
+			if (a[i] <= 0) {
+				continue;
+			}
+			
+			for (int j = i + 1; j < a.length; j++) {
+				if (a[j] <= 0) {
+					continue;
+				}
+				
+				if(a[i] == a[j]) {
+					return -1; // duplicate error 
+				}
+				
+				int checkNumber = a[i] + a[j];
+				if (isPerfectSquare(checkNumber) == 1) {
+					countSquarePairsResult++;
+				}
+			}
+		}
+
+		return countSquarePairsResult;
+
+	}
+
+	static int isPerfectSquare(int n) {
+		int i = 0;
+		int square;
+		do {
+			square = i * i;
+			i++;
+		} while (square < n);
+
+		return square == n ? 1 : 0;
+	}
+}
